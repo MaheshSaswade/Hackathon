6452afae52c7f29e469f88845fbd2e4ba340e842
MaheshSaswade <mahesh.saswade2004@gmail.com>
2019-11-20 20:06:21 +0530

    day end changes....

--- Server/routers/employee.js
+++ Server/routers/employee.js
@@ -3,25 +3,48 @@ const Employee = require('../models/emp')
 // const cors = require('cors')
 const router = new express.Router()
 
+router.get('/employee', async (req,res) => {
+    try {
+        const emps = await Employee.find({});
+        console.log("emps",emps);
+        res.send(emps);
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
 
-router.patch('/emp/:id', async (req, res) => 
-{
+router.post('/employee',async (req,res) => {
+    const employee = new Employee(req.body)
+    try { 
+        await employee.save()
+        // const token = await user.generateAuthToken()
+        // console.log("Hiii Mahesh",token)
+        res.send(employee)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+router.get('/users/me', async (req, res) => {
+    res.send(req.user)
+})
+
+router.patch('/employee/:id', async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['empDesignation', 'empDepartment', 'mobileNo', 'managerID','password']
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
     if (!isValidOperation) {
-            return res.status(400).send({ error: 'Invalid updates!' })
+        return res.status(400).send({ error: 'Invalid updates!' })
     }
     try 
     {
-            const emp = await employee.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
-            if (!emp) {
-                    return res.status(404).send()
-            }
-    
-            res.send(emp)
+        const emp = await employee.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        if (!emp) {
+            return res.status(404).send()
+        }
+        res.send(emp)
     } catch (e) {
-            res.status(400).send(e)
+        res.status(400).send(e)
     }
 })
 
@@ -91,9 +114,7 @@ router.get('/employee', async (req,res) => {
 
 // Get profile
 
-router.get('/users/me', async (req, res) => {
-    res.send(req.user)
-})
+
 
 module.exports = router
 
