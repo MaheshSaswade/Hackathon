5ea527c8a1007548f2a78850e4bfc0f78ce91761
Saurabh Kasar <saurabh.kasar@enzigma.com>
2019-11-20 10:50:57 +0530

    Login logout added

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -1,21 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { EmpDashboardComponent } from './components/emp-dashboard/emp-dashboard.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmpDashboardComponent
+    EmpDashboardComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     RouterModule.forRoot([
     {
       path : 'app-emp-dashboard', component : EmpDashboardComponent
+    },
+    {
+      path: 'login', component: LoginComponent
     }
   ])
   ],
--- Server/index.js
+++ Server/index.js
@@ -1,5 +1,26 @@
 const express = require('express')
+require('./db/mongoose')
 
-if(express) {
-    console.log("Welcome in application")
-}
+const cors = require("cors");
+const bodyParser = require('body-parser')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(bodyParser.json())
+
+app.use(cors())
+
+const userRouter = require('./routers/user')
+
+app.use(userRouter)
+
+app.listen(port, () => {
+    console.log('Server up on ' + port)
+})
+
+ const main = async () => {
+ }
+
+
+main()
