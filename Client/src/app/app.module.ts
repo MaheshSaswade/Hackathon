af33a6e5b6de122988fd26c6c875f12189ba5dbc
Saurabh Kasar <saurabh.kasar@enzigma.com>
2019-11-20 14:35:28 +0530

    Emp dashboard final

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -11,17 +11,19 @@ import { from } from 'rxjs';
 import { AppComponent } from './app.component';
 import { EmpDashboardComponent } from './components/emp-dashboard/emp-dashboard.component';
 import { LoginComponent } from './components/login/login.component';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InsertReviewComponent
+    InsertReviewComponent,
+    EmpDashboardComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
-    EmpDashboardComponent,
-    LoginComponent,
+    FormsModule,
     HttpClientModule,
     RouterModule.forRoot([
     {
@@ -30,10 +32,13 @@ import { LoginComponent } from './components/login/login.component';
     {
       path: 'login', component: LoginComponent
     }
-  ])
+    
+  ]),
+    CollapseModule.forRoot(),
+    BrowserAnimationsModule
   ],
   providers: [],
-  bootstrap: [AppComponent, EmpDashboardComponent. LoginComponent]
+  bootstrap: [AppComponent, EmpDashboardComponent, LoginComponent]
 })
 
 export class AppModule { }
