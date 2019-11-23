acb46c0ef568c4fa47bd495409f066dfe5aad57e
yankita477 <57901274+yankita477@users.noreply.github.com>
2019-11-23 15:31:33 +0530

    c

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -3,27 +3,13 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-=======
 import { CreateEmpComponent } from './components/create-emp/create-emp.component';
->>>>>>> parent of f125e0d... Merge remote-tracking branch 'origin/mahesh' into AnkitaY
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 import { InsertReviewComponent } from './components/review/insert-review/insert-review.component';
 import { CollapseModule } from 'ngx-bootstrap/collapse';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-<<<<<<< HEAD
 import { EmployeeService } from './services/employee.service';
 import { AppRoutingModule, routingComponent} from './RoutingModule/app.routing.module';
-<<<<<<< HEAD
-import { EmployeeProfileService } from './services/employee-profile.service';
-import { RouterModule } from '@angular/router';
-import { EmpDashboardComponent } from './components/emp-dashboard/emp-dashboard.component';
-import { LoginComponent } from './components/login/login.component';
-
-
-=======
->>>>>>> parent of e18024d... display sequence
-=======
 import { HrDashboardComponent } from './components/hr-dashboard/hr-dashboard.component';
 import { ViewProfileComponent } from './components/view-profile/view-profile.component';
 import { ViewReviewsComponent } from './components/view-reviews/view-reviews.component';
@@ -32,24 +18,16 @@ import { AllReviewsListComponent } from './components/hr-dashboard/all-reviews-l
 import { ManageEmpComponent } from './components/hr-dashboard/manage-emp/manage-emp.component';
 import { ManageReviewComponent } from './components/hr-dashboard/manage-review/manage-review.component';
 import { ListEmployeeComponent } from './components/list-employee/list-employee.component';
-import { EmployeeService } from './services/employee.service';
-<<<<<<< HEAD
+import { RouterModule } from '@angular/router';
+import { EmpDashboardComponent } from './components/emp-dashboard/emp-dashboard.component';
+import { LoginComponent } from './components/login/login.component';
 
-<<<<<<< HEAD
->>>>>>> parent of eccf493... Emp View
-=======
->>>>>>> parent of eccf493... Emp View
-=======
-import { AppRoutingModule, routingComponent} from './RoutingModule/app.routing.module';
-import { EmployeeProfileService } from './services/employee-profile.service';
->>>>>>> parent of f125e0d... Merge remote-tracking branch 'origin/mahesh' into AnkitaY
 @NgModule({
   declarations: [
     AppComponent,
     InsertReviewComponent,
-<<<<<<< HEAD
+
     routingComponent ,   
-=======
     EmpDashboardComponent,
     LoginComponent,
     HrDashboardComponent,
@@ -59,25 +37,16 @@ import { EmployeeProfileService } from './services/employee-profile.service';
     AllReviewsListComponent,
     ManageEmpComponent,
     ManageReviewComponent,
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-    ListEmployeeComponent
-<<<<<<< HEAD
->>>>>>> parent of eccf493... Emp View
-=======
->>>>>>> parent of eccf493... Emp View
-=======
+
+    ListEmployeeComponent,
+
     routingComponent    
->>>>>>> parent of f125e0d... Merge remote-tracking branch 'origin/mahesh' into AnkitaY
   ],
   imports: [
     BsDatepickerModule.forRoot(),
     BrowserModule,
     FormsModule,
     HttpClientModule,
-<<<<<<< HEAD
-=======
     RouterModule.forRoot([
     {
         path: 'login', component: LoginComponent
@@ -107,18 +76,11 @@ import { EmployeeProfileService } from './services/employee-profile.service';
       path: 'managereview', component: ManageReviewComponent
     }
   ]),
->>>>>>> parent of f125e0d... Merge remote-tracking branch 'origin/mahesh' into AnkitaY
     CollapseModule.forRoot(),
     BrowserAnimationsModule,
   ],
-<<<<<<< HEAD
-  providers: [EmployeeService, EmployeeProfileService],
-  bootstrap: [AppComponent, EmpDashboardComponent, LoginComponent],
-  exports: [RouterModule]
-=======
   providers: [EmployeeService],
   bootstrap: [AppComponent, EmpDashboardComponent, LoginComponent]
->>>>>>> parent of e18024d... display sequence
 })
 
 export class AppModule { }
--- Client/src/app/services/employee.service.ts
+++ Client/src/app/services/employee.service.ts
@@ -1,25 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { Observable, from } from 'rxjs';
-// import 'rxjs/add/operator/map';
-// import 'rxjs/add/operator/toPromise';
-import { CreateEmpComponent } from '../components/hr-dashboard/manage-emp/create-emp/create-emp.component';
-import { UpdateEmpComponent } from '../components/hr-dashboard/manage-emp/update-emp/update-emp.component';
-import { DeleteEmpComponent } from '../components/hr-dashboard/manage-emp/delete-emp/delete-emp.component';
-=======
-// import { Observable } from 'rxjs';
-// import { EmployeeInterface } from '../EmployeeInterface';
->>>>>>> parent of e18024d... display sequence
-=======
->>>>>>> parent of f125e0d... Merge remote-tracking branch 'origin/mahesh' into AnkitaY
-=======
->>>>>>> parent of f125e0d... Merge remote-tracking branch 'origin/mahesh' into AnkitaY
-=======
->>>>>>> parent of f125e0d... Merge remote-tracking branch 'origin/mahesh' into AnkitaY
+
 
 @Injectable({
   providedIn: 'root'
@@ -30,64 +11,21 @@ export class EmployeeService {
   
   
   private url = "http://localhost:3000/employee";
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-//   selectedEmployee: CreateEmpComponent;
-//   employees: CreateEmpComponent[];
-//  readonly baseUrl = 'http//localhost:3000/employee';
-  
+
   constructor(private http: HttpClient) { }
 
-<<<<<<< HEAD
-=======
 //   getemployees():Observable<EmployeeInterface[]>{
 //     return this.http.get<EmployeeInterface[]>(this.url);
 // }
 
->>>>>>> parent of e18024d... display sequence
   public  getemployees()  {
     return this.http.get(this.url)
   }
-  // postEmployee(emp: CreateEmpComponent) {
-  //   return this.http.post(this.baseUrl + 'employee', emp);
-  // }
 
-  // putEmployee(upEmp: UpdateEmpComponent) {
-  //   return this.http.put(this.baseUrl + '/emp/:id', upEmp );
-  // }
-=======
-  
-  constructor(private http: HttpClient) { }
-
-
-
-  public  getemployees()  {
-    return this.http.get(this.url)
-  }
->>>>>>> parent of f125e0d... Merge remote-tracking branch 'origin/mahesh' into AnkitaY
-=======
-  
-  constructor(private http: HttpClient) { }
-
-
-=======
-  
-  constructor(private http: HttpClient) { }
-
-
-
-  public  getemployees()  {
-    return this.http.get(this.url)
-  }
->>>>>>> parent of f125e0d... Merge remote-tracking branch 'origin/mahesh' into AnkitaY
-
-  public  getemployees()  {
-    return this.http.get(this.url)
-  }
->>>>>>> parent of f125e0d... Merge remote-tracking branch 'origin/mahesh' into AnkitaY
-
-  // deleteEmployee(delEmp) {
-  //   return this.http.delete(this.baseUrl + '/emp/:email', delEmp );
-  // }
 }
+  
+
+
+  
+  
+
