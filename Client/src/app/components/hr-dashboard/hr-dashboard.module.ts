f58c52f841a2fc181ace1ec74c4fbd1d7f1f3bfc
Saurabh Kasar <saurabh.kasar@enzigma.com>
2019-11-25 11:27:19 +0530

    review component shifted

--- Client/src/app/components/hr-dashboard/hr-dashboard.module.ts
+++ Client/src/app/components/hr-dashboard/hr-dashboard.module.ts
@@ -4,14 +4,15 @@ import { HrDashboardComponent } from './hr-dashboard.component';
 import { AllReviewsListComponent } from './all-reviews-list/all-reviews-list.component';
 import { ManageEmpComponent } from './manage-emp/manage-emp.component';
 import { ManageReviewComponent } from './manage-review/manage-review.component';
+import { ViewStatsComponent } from './view-stats/view-stats.component';
 
 
 
 @NgModule({
-  declarations: [AllReviewsListComponent, ManageReviewComponent],
+  declarations: [AllReviewsListComponent, ManageReviewComponent, ViewStatsComponent],
   imports: [
     CommonModule
   ],
-  exports: [AllReviewsListComponent, ManageReviewComponent]
+  exports: [AllReviewsListComponent, ManageReviewComponent, ViewStatsComponent]
 })
 export class HrDashboardModule { }
